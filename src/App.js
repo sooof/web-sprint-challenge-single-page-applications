1c19deb40405ec92902ae6d1b958b4330401736f
sooof <sooof@me.com>
2021-10-15 09:02:26 -0700

    Add nav and First Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import {Route, Link, Switch} from 'react-router-dom'
+import './App.css'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='App'>
+      <nav className='App-header'>
+          <h1>Lambda Eats</h1>
+          
+          <div className='App-link'>
+            <Link to='/'>Home</Link>
+            <Link to='/items-list'>Help</Link>
+          </div>
+      </nav>
+
+      <Switch>
+        <Route exact path='/'>
+          {/* <Home/> */}
+        </Route>
+        <Route>
+
+        </Route>
+      </Switch>
+    </div>
+ 
   );
 };
 export default App;
